84b1a38349e8e2bfa3140ad14e4fa89c98f0742f
ma1VAR3 <prajjwalgupta1465@gmail.com>
2021-10-18 19:44:12 +0530

    update

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
 
 const Header = (props) => {
+  const [search, setSearch] = useState(false);
+
+  const changeFocus = () => {
+    setSearch(!search);
+  };
+
+  const searchClass = search ? "class-inp active" : "class-inp";
+  const searchIconClass = search
+    ? "bi bi-search search-icon active"
+    : "bi bi-search search-icon inactive";
+
   return (
     <header className="header">
       <div className="navbar row">
@@ -21,10 +32,22 @@ const Header = (props) => {
       <div className="row search-container justify-content-center">
         <div className="col-6 search">
           <input
+            onFocus={changeFocus}
+            onBlur={changeFocus}
             type="text"
             placeholder="search the library..."
-            className="class-inp"
+            className={searchClass}
           />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            class={searchIconClass}
+            viewBox="0 0 16 16"
+          >
+            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+          </svg>
         </div>
       </div>
     </header>
