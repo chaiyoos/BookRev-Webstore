472651d57099ea7bb37c212dccbbc6142008399a
ma1VAR3 <prajjwalgupta1465@gmail.com>
2021-10-18 20:27:47 +0530

    update

--- frontend/src/containers/Home/index.js
+++ frontend/src/containers/Home/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Hero from "../../components/Hero";
+import Info from "../../components/Info";
 import "./style.css";
 
 const Home = (props) => {
   return (
     <div>
       <Hero />
+      <Info />
     </div>
   );
 };
