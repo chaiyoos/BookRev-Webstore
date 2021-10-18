b83eac5d9c0ed301201e579b11eadba5d8a9775b
ma1VAR3 <prajjwalgupta1465@gmail.com>
2021-10-18 20:39:20 +0530

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 import Header from "./components/Header";
 import Home from "./containers/Home";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Home />
+      <Footer />
     </div>
   );
 }
